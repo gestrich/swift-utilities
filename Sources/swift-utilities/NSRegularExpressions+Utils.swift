a7b8a71e74b920ff93802ce7e967fe4fdae17c2e
Bill Gestrich <bill@foreflight.com>
2019-11-17 12:02:13 -0500

    add method

--- Sources/swift-utilities/NSRegularExpressions+Utils.swift
+++ Sources/swift-utilities/NSRegularExpressions+Utils.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public extension NSRegularExpression {
+    //TODO: For multi-line regex, this returns the last line currently.
     func stringMatch (toSearch: String, matchIndex: Int) -> String? {
         let nsstr = toSearch as NSString
         let all = NSRange(location: 0, length: nsstr.length)
@@ -23,6 +24,23 @@ public extension NSRegularExpression {
         }
             
         return match
+    }
+    
+    func stringMatches (toSearch: String, matchIndex: Int) -> [String] {
+        let nsstr = toSearch as NSString
+        let all = NSRange(location: 0, length: nsstr.length)
+        var matches = [String]()
+        self.enumerateMatches(in: toSearch, options: [], range: all) {
+            (result : NSTextCheckingResult?, _, _) in
+            if let r = result {
+                if r.range(at: matchIndex).location != NSNotFound {
+                    let result = nsstr.substring(with: r.range(at: matchIndex)) as String
+                    matches.append(result)
+                }
+            }
+        }
+            
+        return matches
     }
     
     func floatMatch (toSearch: String, matchIndex : Int) -> Float? {
