5afef61e761d7b3e06ece6ef3356abb6b5a0d9dc
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2021-12-09 07:16:54 -0500

    Add public initializer

--- Sources/swift-utilities/CommandLinePromptAction.swift
+++ Sources/swift-utilities/CommandLinePromptAction.swift
@@ -10,4 +10,10 @@ import Foundation
 public struct CommandLinePromptAction {
     public let name: String
     public let action: () throws -> Void
+    
+    public init(name: String, action: @escaping () throws -> Void) {
+        self.name = name
+        self.action = action
+    }
+    
 }
